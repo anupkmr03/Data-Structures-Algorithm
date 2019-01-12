59301496b8f4edad93706d2214d95f2aa9ba6784
Anup <anup.kmr03@gmail.com>
2019-01-13 03:12:34 +0530

    Update MostOftenUsedIp.java
    corrected the Most frequently logic

--- string/MostOftenUsedIp.java
+++ string/MostOftenUsedIp.java
@@ -1,50 +1,70 @@
 package string;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 
 public class MostOftenUsedIp {
     public static void main(String[] args) {
-	String[] ips = { "10.0.0.1 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326",
-		"10.0.0.1 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326",
-		"10.0.0.2 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326" };
+        String[] ips = {"10.0.0.1 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326",
+                "10.0.0.1 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326",
+                "10.0.0.2 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326",
+                "10.0.0.1 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326",
+                "10.0.0.3 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326",
+                "10.0.0.3 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326",
+                "10.0.0.1 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326",
+        };
 
-	String result = getMostOftenUsedIP(ips);
-	System.out.println(result);
+       getMostOftenUsedIP(ips,3);
 
     }
 
-    private static String getMostOftenUsedIP(String[] ips) {
-	Map<String, Integer> map = new HashMap<>();
+    private static void getMostOftenUsedIP(String[] ips,int topResult) {
+        Map<String, Integer> map = new HashMap<>();
 
-	for (int i = 0; i < ips.length; i++) {
-	    String temp = ips[i];
-	    String ip = temp.split(" ")[0];
+        for (int i = 0; i < ips.length; i++) {
+            String temp = ips[i];
+            String ip = temp.split(" ")[0];
 
-	    if (map.containsKey(ip)) {
-		map.put(ip, map.get(ip) + 1);
-	    } else {
-		map.put(ip, 1);
-	    }
-	}
+            if (map.containsKey(ip)) {
+                map.put(ip, map.get(ip) + 1);
+            } else {
+                map.put(ip, 1);
+            }
+        }
 
-	Set<Entry<String, Integer>> set = map.entrySet();
-	List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(set);
-	Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-	    public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-		return (o2.getValue()).compareTo(o1.getValue());
-	    }
-	});
+        List<Entry<String, Integer>> output = findGreatest(map, topResult);
 
-	Entry<String, Integer> entry = list.get(0);
+        System.out.println("Showing Top "+topResult+" Results");
+        for (int i = 0; i <topResult; i++) {
+            System.out.println("IP :"+output.get(i).getKey() + " :: Occurrence Count :" +output.get(i).getValue());
+        }
 
-	return entry.getKey();
     }
 
+
+    private static <K, V extends Comparable<? super V>> List<Entry<K, V>> findGreatest(Map<K, V> map, int n) {
+        Comparator<? super Entry<K, V>> comparator =
+                new Comparator<Entry<K, V>>() {
+                    @Override
+                    public int compare(Entry<K, V> e0, Entry<K, V> e1) {
+                        V v0 = e0.getValue();
+                        V v1 = e1.getValue();
+                        return v1.compareTo(v0);//reverse order
+                    }
+                };
+
+
+        PriorityQueue<Entry<K, V>> heap =
+                new PriorityQueue<>(map.size(), comparator);
+
+        for (Entry<K, V> entry : map.entrySet()) {
+            heap.add(entry);
+        }
+
+        List<Entry<K, V>> result = new ArrayList<Map.Entry<K, V>>();
+        while (heap.size() > 0 && n >= 0) {
+            result.add(heap.poll());
+        }
+        return result;
+    }
 }
