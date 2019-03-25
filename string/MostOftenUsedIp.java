217535896019b301a5f694e9bc9389106f588fef
Anup <anup.kmr03@gmail.com>
2019-03-25 23:45:52 +0530

    Update MostOftenUsedIp.java
    code refactoring

--- string/MostOftenUsedIp.java
+++ string/MostOftenUsedIp.java
@@ -1,7 +1,10 @@
 package string;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
+import java.util.PriorityQueue;
 
 public class MostOftenUsedIp {
     public static void main(String[] args) {
@@ -14,12 +17,12 @@ public class MostOftenUsedIp {
                 "10.0.0.1 - google [10/Oct/2000:13:55:36 -0700] GET /apache_pb.gif HTTP/1.0 200 2326",
         };
 
-       getMostOftenUsedIP(ips,3);
+        getMostOftenUsedIP(ips, 3);
 
     }
 
-    private static void getMostOftenUsedIP(String[] ips,int topResult) {
-        Map<String, Integer> map = new HashMap<>();
+    private static void getMostOftenUsedIP(String[] ips, int topResult) {
+        HashMap<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < ips.length; i++) {
             String temp = ips[i];
@@ -34,34 +37,21 @@ public class MostOftenUsedIp {
 
         List<Entry<String, Integer>> output = findGreatest(map, topResult);
 
-        System.out.println("Showing Top "+topResult+" Results");
-        for (int i = 0; i <topResult; i++) {
-            System.out.println("IP :"+output.get(i).getKey() + " :: Occurrence Count :" +output.get(i).getValue());
+        System.out.println("Showing Top " + topResult + " Results");
+        for (int i = 0; i < topResult; i++) {
+            System.out.println("IP :" + output.get(i).getKey() + " :: Occurrence Count :" + output.get(i).getValue());
         }
 
     }
 
 
-    private static <K, V extends Comparable<? super V>> List<Entry<K, V>> findGreatest(Map<K, V> map, int n) {
-        Comparator<? super Entry<K, V>> comparator =
-                new Comparator<Entry<K, V>>() {
-                    @Override
-                    public int compare(Entry<K, V> e0, Entry<K, V> e1) {
-                        V v0 = e0.getValue();
-                        V v1 = e1.getValue();
-                        return v1.compareTo(v0);//reverse order
-                    }
-                };
+    private static List<Entry<String, Integer>> findGreatest(HashMap<String, Integer> map, int n) {
 
+        PriorityQueue<Entry<String, Integer>> heap = new PriorityQueue<>(map.size(), (o1, o2) -> 0);
 
-        PriorityQueue<Entry<K, V>> heap =
-                new PriorityQueue<>(map.size(), comparator);
+        heap.addAll(map.entrySet());
 
-        for (Entry<K, V> entry : map.entrySet()) {
-            heap.add(entry);
-        }
-
-        List<Entry<K, V>> result = new ArrayList<Map.Entry<K, V>>();
+        List<Entry<String, Integer>> result = new ArrayList<>();
         while (heap.size() > 0 && n >= 0) {
             result.add(heap.poll());
         }
