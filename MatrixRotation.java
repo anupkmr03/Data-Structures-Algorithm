11216c4de4829480cc5229a853208eb7daf1ab2c
Anup <anup.kmr03@gmail.com>
2019-10-07 01:04:28 +0530

    Update MatrixRotation.java

--- MatrixRotation.java
+++ MatrixRotation.java
@@ -3,37 +3,32 @@ import java.util.*;
 public class MatrixRotation { 
 
 	
-	static void reverseColumns(int a[][]) 
-	{ 
-		for (int i = 0; i < a[0].length; i++) 
-        {
-        	int start = 0;
-            int end = a[0].length-1;
-          	
-          	while(start<=end){
-              swap2(a,i,start,end);
-              start++;
-              end--;
-            	
-          	}
-		
-        } 
+	static void reverseColumns(int a[][]) { 
+		for (int i = 0; i < a[0].length; i++) {
+			int start = 0;
+			int end = a[0].length-1;
+
+			while(start<=end){
+			      antiClockWiseSwap(a,i,start,end);
+			      start++;
+			      end--;
+			}
+             	} 
 	} 
   
   static void antiClockWiseSwap(int a[][], int k, int i, int j ){
-   			int temp = a[i][k];
+   		int temp = a[i][k];
     		a[i][k] = a[j][k];
     		a[j][k] = temp;
   }
   static void clockWiseSwap(int a[][], int k, int i, int j ){
-   			int temp = a[k][i];
+   		int temp = a[k][i];
     		a[k][i] = a[k][j];
     		a[k][j] = temp;
   }
 
 	// Function for do transpose of matrix 
-	static void transpose(int arr[][]) 
-	{ 
+	static void transpose(int arr[][]) { 
 		for (int i = 0; i < arr.length; i++) 
 			for (int j = i; j < arr[0].length; j++) { 
 				int temp = arr[j][i]; 
@@ -59,6 +54,21 @@ public class MatrixRotation {
 		transpose(arr); 
 		reverseColumns(arr); 
 	} 
+	
+	//**** Diff Approch for rotating matrix ******//
+	private static void rotateAntiClockWiseOtherApproch(int[][] a, int n) {
+        int noOfRotations = n / 2;
+        n = n - 1;
+        for (int i = 0; i < noOfRotations; i++) {
+            for (int j = 0; j < n - i; j++) {
+                int temp = a[i][j];
+                a[i][j] = a[j][n - i];
+                a[j][n - i] = a[n - i][n - j];
+                a[n - i][n - j] = a[n - j][i];
+                a[n - j][i] = temp;
+            }
+        }
+    }
 
 	/* Driver program to test above function */
 	public static void main(String[] args) 
